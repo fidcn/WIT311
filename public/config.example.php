bcc4e6432a577e75a6dc74d7aafb821dc5e412a5
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-04-08 19:58:37 -0400

    Updated example config

---
+++ public/config.example.php
@@ -0,0 +1,17 @@
+<?php
+	// DB Connection address
+	define('DB_HOST', "127.0.0.1");
+// DB Connection port
+	define('DB_PORT', 3306);
+// DB User username
+	define('DB_USER', "wit311");
+// DB User password
+	define('DB_PASS', "secret");
+// Selected databse
+	define('DB_DATA', "wit311");
+
+// Name of instance
+	define('INST_NAME', "WIT311");
+// Location served by instance
+	define('INST_LOC', "BOSTON, MA");
+?>
